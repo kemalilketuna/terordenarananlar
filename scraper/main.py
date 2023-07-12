1ea28e8c779e2b5224094f61f35f4b15e4db45b0
Kemal İlke Tuna <35730029+kemalilketuna@users.noreply.github.com>
2023-07-12 17:34:38 +0300

    end of the day

--- scraper/main.py
+++ scraper/main.py
@@ -2,7 +2,7 @@ from time import sleep
 import requests
 import logging
 import os
-import io
+import json
 
 # wait for the database to start
 # sleep(10) # remove this later
@@ -24,28 +24,98 @@ logging.basicConfig(
 logging.info("Service started.")
 
 
-def _get_neutralized_terrorist_info():
-    pass
+def get_neutralized_terrorist_info():
+    response = requests.post(
+        "https://www.terorarananlar.pol.tr/ISAYWebPart/TArananlar/GetEtkisizList"
+    )
+    return response.json()
 
 
-def _get_wanted_terrorist_info():
-    pass
+def get_wanted_terrorist_info():
+    response = requests.post(
+        "https://www.terorarananlar.pol.tr/ISAYWebPart/TArananlar/GetTerorleArananlarList"
+    )
+    return response.json()
 
 
-def new_record():
-    pass
+# change it
+def _insert_to_db(session, table, person_db):
+    new_row = table.insert().values(person_db)
+    session.execute(new_row)
 
 
-def photo_url_creator():
-    pass
+def get_info_from_db(session, table, person_db):
+    stm = table.select().where(
+        table.c.name == person_db["name"]
+        and table.c.surname == person_db["surname"]
+        and table.c.birth_year == person_db["birth_year"]
+        and table.c.birth_place == person_db["birth_place"]
+    )
+    result = session.execute(stm)
+    if result.rowcount == 0:
+        return None
+    else:
+        return result.fetchone()
 
 
-def minio_name_genator():
-    pass
+def photo_url_creator(photo_url):
+    base_url = "https://www.terorarananlar.pol.tr"
+    return base_url + photo_url
+
+
+def _minio_name_genator(name, surname, photo_url):
+    end_part = photo_url.split("/")[-1]
+    return f"{name}_{surname}_{end_part}"
+
+
+def minio_url_ceator(name, surname, photo_url):
+    base_url = "http://localhost:9000"
+    return f"{base_url}/{BUCKET_NAME}/{_minio_name_genator(name, surname, photo_url)}"
+
+
+def upload_photo_to_minio(name, surname, photo_url):
+    minio_name = _minio_name_genator(name, surname, photo_url)
+    url = photo_url_creator(photo_url)
+    response = requests.get(url)
+    send_photo(response, minio_name)
+
+
+def _adapter(wanted_person, isactive):
+    adapter = {}
+    adapter["name"] = wanted_person["Adi"]
+    adapter["surname"] = wanted_person["Soyadi"]
+    adapter["birth_year"] = int(wanted_person["DogumTarihi"])
+    adapter["birth_place"] = wanted_person["DogumYeri"]
+    adapter["terrorist_organization"] = wanted_person["TOrgutAdi"]
+    adapter["category"] = wanted_person["TKategoriAdi"]
+    adapter["isactive"] = isactive
+
+    photos_db = []
+    for photo_url in wanted_person["GorselURL"]:
+        photos_db.append(
+            minio_url_ceator(adapter["name"], adapter["surname"], photo_url)
+        )
+        upload_photo_to_minio(adapter["name"], adapter["surname"], photo_url)
+    adapter["photos"] = json.dumps(photos_db)
+    return adapter
 
 
 def fetch():
-    pass
+    table = get_table()
+    session = get_db_session()
+
+    wanted_list = get_wanted_terrorist_info()
+    neutralized_list = get_neutralized_terrorist_info()
+
+    for category in wanted_list.values():
+        for wanted_person in category:
+            person_db = _adapter(wanted_person, True)
+            _insert_to_db(session, table, person_db)
+
+    for category in neutralized_list.values():
+        for neutralized_person in category:
+            _adapter(neutralized_person, False)
+            _insert_to_db(session, table, person_db)
 
 
 if __name__ == "__main__":
