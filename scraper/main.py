a00b7271bd5eaa4ac927cf1e7d570a9f41ecec88
Kemal İlke Tuna <35730029+kemalilketuna@users.noreply.github.com>
2023-07-13 17:26:43 +0300

    something

--- scraper/main.py
+++ scraper/main.py
@@ -7,13 +7,11 @@ import json
 # wait for the database to start
 # sleep(10) # remove this later
 
-from database import get_db_session, get_table, get_info_from_db
-from minio_utils import send_photo
+from database import get_info_from_db, _insert_to_db
+from minio_utils import send_photo, _minio_name_genator, _minio_url_ceator
 from dotenv import load_dotenv  # remove this later
 
 load_dotenv()
-
-BUCKET_NAME = os.environ["BUCKET_NAME"]
 
 logging.basicConfig(
     filename="scraper.log",
@@ -38,25 +36,9 @@ def get_wanted_terrorist_info():
     return response.json()
 
 
-# change it
-def _insert_to_db(session, table, person_db):
-    new_row = table.insert().values(person_db)
-    session.execute(new_row)
-
-
 def photo_url_creator(photo_url):
     base_url = "https://www.terorarananlar.pol.tr"
     return base_url + photo_url
-
-
-def _minio_name_genator(name, surname, photo_url):
-    end_part = photo_url.split("/")[-1]
-    return f"{name}_{surname}_{end_part}"
-
-
-def minio_url_ceator(name, surname, photo_url):
-    base_url = "http://localhost:9000"
-    return f"{base_url}/{BUCKET_NAME}/{_minio_name_genator(name, surname, photo_url)}"
 
 
 def upload_photo_to_minio(name, surname, photo_url):
@@ -66,7 +48,7 @@ def upload_photo_to_minio(name, surname, photo_url):
     send_photo(response, minio_name)
 
 
-def insert_to_db(session, table, person, isactive):
+def insert_to_db(person, isactive):
     adapter = {}
     adapter["name"] = person["Adi"]
     adapter["surname"] = person["Soyadi"]
@@ -85,34 +67,29 @@ def insert_to_db(session, table, person, isactive):
     else:
         for photo_url in person["GorselURL"]:
             photos_db.append(
-                minio_url_ceator(adapter["name"], adapter["surname"], photo_url)
+                _minio_url_ceator(adapter["name"], adapter["surname"], photo_url)
             )
             upload_photo_to_minio(adapter["name"], adapter["surname"], photo_url)
         adapter["photos"] = json.dumps(photos_db, ensure_ascii=False)
 
-    _insert_to_db(session, table, adapter)
+    _insert_to_db(adapter)
 
 
-def update_person_db(session, table, person, person_db, isactive):
+def update_person_db(person, person_db, isactive):
     pass
 
 
 def fetch():
-    table = get_table()
-    session = get_db_session()
-
     wanted_list = get_wanted_terrorist_info()
     neutralized_list = get_neutralized_terrorist_info()
 
     for category in neutralized_list.values():
         for neutralized_person in category:
-            insert_to_db(session, table, neutralized_person, False)
+            insert_to_db(neutralized_person, False)
 
     for category in wanted_list.values():
         for wanted_person in category:
-            insert_to_db(session, table, wanted_person, True)
-
-    session.close()
+            insert_to_db(wanted_person, True)
 
 
 if __name__ == "__main__":
